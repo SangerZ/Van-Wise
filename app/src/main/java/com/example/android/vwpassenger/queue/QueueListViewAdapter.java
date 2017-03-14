24a32d04232787869b9ec84617cc87c6e5e07818
Pulitz Lertamornthep <pulitz@hotmail.com>
2017-03-14 10:58:05 +0700

    enqueue dequeue

--- app/src/main/java/com/example/android/vwpassenger/queue/QueueListViewAdapter.java
+++ app/src/main/java/com/example/android/vwpassenger/queue/QueueListViewAdapter.java
@@ -1,6 +1,7 @@
 package com.example.android.vwpassenger.queue;
 
 import android.content.Context;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -8,14 +9,25 @@ import android.widget.BaseAdapter;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.android.vwpassenger.R;
+import com.example.android.vwpassenger.session.SessionManager;
+import com.example.android.vwpassenger.web_service.VanWiseAPIClient;
+import com.example.android.vwpassenger.web_service.VanWiseInterface;
+
+import java.util.ArrayList;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 /**
  * Created by Pulitz on 3/4/2017.
  */
 
 public class QueueListViewAdapter extends BaseAdapter {
+    private final VanWiseInterface api = VanWiseAPIClient.getClient().create(VanWiseInterface.class);
     private Context mContext;
     private LayoutInflater mInflater;
     private QueueDataSource dataSource;
@@ -39,7 +51,7 @@ public class QueueListViewAdapter extends BaseAdapter {
 
     @Override
     public long getItemId(int i) {
-        return i;
+        return dataSource.get(i).getTripID();
     }
 
     @Override
@@ -87,6 +99,70 @@ public class QueueListViewAdapter extends BaseAdapter {
                     notifyDataSetChanged();
                 }
             });
+        }
+
+        view.findViewById(R.id.lv_body).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int userTripID = dataSource.getUserTripID();
+                final int tripID = t.getTripID();
+                if (userTripID == 0 || userTripID != tripID) {
+                    Call<EnqueueResponse> call = api.Enqueue(SessionManager.getInstance().getSessionDetails().getUserID(), tripID);
+                    call.enqueue(new Callback<EnqueueResponse>() {
+                        @Override
+                        public void onResponse(Call<EnqueueResponse> call, Response<EnqueueResponse> response) {
+                            Log.d("Testing Enqueue", response.toString());
+                            EnqueueResponse res = response.body();
+                            if (res.isSuccess()) {
+                                Toast.makeText(mContext, res.getMessage(), Toast.LENGTH_SHORT).show();
+                                dataSource.update((ArrayList<Queue>) res.getQueueUpdates());
+                                dataSource.setUserTripID(tripID);
+                                notifyDataSetChanged();
+                            } else {
+                                Toast.makeText(mContext, res.getMessage(), Toast.LENGTH_SHORT).show();
+                            }
+                        }
+
+                        @Override
+                        public void onFailure(Call<EnqueueResponse> call, Throwable t) {
+                            Toast.makeText(mContext, "Failure to enqueue: " + t.getMessage(), Toast.LENGTH_SHORT).show();
+                        }
+                    });
+
+                } else {
+                    Call<EnqueueResponse> call = api.Dequeue(SessionManager.getInstance().getSessionDetails().getUserID());
+                    call.enqueue(new Callback<EnqueueResponse>() {
+                        @Override
+                        public void onResponse(Call<EnqueueResponse> call, Response<EnqueueResponse> response) {
+                            Log.d("Testing Dequeue", response.toString());
+                            EnqueueResponse res = response.body();
+                            if (res.isSuccess()) {
+                                Toast.makeText(mContext, res.getMessage(), Toast.LENGTH_SHORT).show();
+                                dataSource.update((ArrayList<Queue>) res.getQueueUpdates());
+                                dataSource.setUserTripID(0);
+                                notifyDataSetChanged();
+                            } else {
+                                Toast.makeText(mContext, res.getMessage(), Toast.LENGTH_SHORT).show();
+                            }
+                        }
+
+                        @Override
+                        public void onFailure(Call<EnqueueResponse> call, Throwable t) {
+                            Toast.makeText(mContext, "Failure to dequeue: " + t.getMessage(), Toast.LENGTH_SHORT).show();
+                        }
+                    });
+
+                }
+            }
+        });
+
+        Log.d("Trip ID Comparison", t.getTripID() + " == " + dataSource.getUserTripID());
+
+        //TODO: Find a better way to implement background colors and Enqueue/Dequeue button
+        if (t.getTripID() == dataSource.getUserTripID()) {
+            view.findViewById(R.id.lv_bg).setBackgroundColor(mContext.getResources().getColor(R.color.blue));
+        } else {
+            view.findViewById(R.id.lv_bg).setBackgroundColor(mContext.getResources().getColor(R.color.white));
         }
 
         return view;
